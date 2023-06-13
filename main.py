7aaec721f91a7631b7a4fce25cb77dcc742d46a6
mohammad reza hassani <mrezahassani@protonmail.com>
2023-06-13 14:42:58 +0330

    get app running

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
+from typing import List
+import time
+
+
 if __name__ == "__main__":
     from utils import get_fullname_and_student_number
+    from utils.input import clear
     from ui import SingUp
     from data import student
 
-    data = get_fullname_and_student_number(60, 14)
+    errors: List[str] = []
 
-    sign_up = SingUp()
-    signup_data: dict[str, str] = sign_up.run()  # type: ignore
-    sign_up.__dict__.update(data)
-    student.login(signup_data)
+    while True:
+        data = get_fullname_and_student_number(60, 14)
+        if len(data["username"].split(" ")) > 1:
+            errors.append("username must not contain spaces")
+
+        try:
+            user = student.login(data["username"], data["password"])
+            if user is None and not data["password"].isdigit():
+                errors.append(
+                    """you are loging is for first time,
+your password is your student code, and must be number"""
+                )
+
+            if len(errors) != 0:
+                clear()
+
+                for err in errors:
+                    print(err)
+
+                print("waiting 5 secons, you will be back to login page")
+                time.sleep(5)
+                continue
+
+            if user is None:
+                sign_up = SingUp()
+                signup_data: dict[str, str] = sign_up.run()  # type: ignore
+
+                signup_data.update(data)
+
+                student.sign_up(signup_data)
+            else:
+                print(user)
+                break
+
+        except Exception as err:
+            clear()
+            print(err)
+            print("\n \nwaiting 5 secons, you will be back to login page")
+            time.sleep(5)
