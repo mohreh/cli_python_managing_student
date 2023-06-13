fba3698802ff994cdaa2659ef5ecef0e2375f740
mohammad reza hassani <mrezahassani@protonmail.com>
2023-06-13 10:17:11 +0330

    more future usable utils module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,16 @@
 from .input import Input
 
 
-def get_fullname_and_student_number(width: int, height: int):
-    fullname = "Enter your fullname first"
-    student_code = "Enter your student_code then"
-
-    input = Input(width, height, fullname, student_code)
+def get_fullname_and_student_number(width: int, height: int) -> tuple[str, str, str]:
+    input = Input(
+        width,
+        height,
+        fullname="Enter your fullname first",
+        password="Enter your student_code/password then",
+        username="Enter your username then",
+    )
 
     input.lisenter.start()
     input.lisenter.join()
 
-    return (input.fullname, input.student_code)
+    return (input.fullname, input.password, input.username)  # type: ignore
--- utils/input.py
+++ utils/input.py
@@ -10,20 +10,20 @@ class Input:
     action: Action = Action.INSERT
     word = ""
 
-    def __init__(
-        self, width: int, height: int, fullname: str, student_code: str
-    ) -> None:
+    def __init__(self, width: int, height: int, **kwargs) -> None:
         self.lisenter = Listener(on_release=self.on_release)
 
         self.width = width
         self.height = height
 
-        self.fullname = fullname
-        self.student_code = student_code
+        # self.fullname = fullname
+        # self.student_code = student_code
+        self.__dict__.update(kwargs)
+        self.to_input = list(kwargs.keys())
 
         self.hits = 0
 
-        print_star_input_box(self.width, self.height, self.fullname, self.student_code)
+        print_star_input_box(self.width, self.height, **kwargs)
 
     def on_release(self, key):
         if key == Key.enter:
@@ -35,58 +35,47 @@ class Input:
         else:
             self.word = self.word + "{}".format(key)[1:2]
 
-        if self.hits == 0:
-            self.fullname = self.word
+        if self.action == Action.COMPLETE and self.hits == len(self.to_input) - 1:
+            return False
 
-            if self.action == Action.COMPLETE:
-                self.hits = 1
-                self.word = ""
-                self.student_code = self.word
-                self.action = Action.INSERT
-        else:
-            self.student_code = self.word
-            if self.action == Action.COMPLETE:
-                return False
+        for index, key in enumerate(self.to_input):
+            if self.hits == index:
+                self.__dict__[key] = self.word
+                if self.action == Action.COMPLETE:
+                    self.hits += 1
+                    self.word = ""
+                    self.__dict__[self.to_input[self.hits]] = self.word
+                    self.action = Action.INSERT
 
-        print_star_input_box(self.width, self.height, self.fullname, self.student_code)
+        values = []
+        for key in self.to_input:
+            values.append(self.__dict__[key])
+
+        print_star_input_box(
+            self.width, self.height, **dict(zip(self.to_input, values))
+        )
 
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 
-def print_star_input_box(width: int, height: int, fullname, student_code):
+def print_star_input_box(width: int, height: int, **kwargs):
     clear()
     print("*" * width)
 
     for _ in range(int(height / 2 - 1)):
         print("*" + " " * (width - 2) + "*")
 
-    print(
-        "*"
-        + " " * (int(width / 2 - 1) - int(len(fullname) / 2))
-        + fullname
-        + " "
-        * (
-            int(width / 2 - 1)
-            - int(len(fullname) / 2)
-            - (1 if len(fullname) % 2 == 1 else 0)
+    for val in kwargs.values():
+        print(
+            "*"
+            + " " * (int(width / 2 - 1) - int(len(val) / 2))
+            + val
+            + " "
+            * (int(width / 2 - 1) - int(len(val) / 2) - (1 if len(val) % 2 == 1 else 0))
+            + "*"
         )
-        + "*"
-    )
-
-    print(
-        "*"
-        + " " * (int(width / 2 - 1) - int(len(student_code) / 2))
-        + student_code
-        + " "
-        * (
-            int(width / 2 - 1)
-            - int(len(student_code) / 2)
-            - (1 if len(student_code) % 2 == 1 else 0)
-        )
-        + "*"
-    )
 
     for _ in range(int(height / 2 - 1)):
         print("*" + " " * (width - 2) + "*")
